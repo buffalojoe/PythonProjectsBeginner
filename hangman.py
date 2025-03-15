f9d6d55f15931cdecb71eb920e4c4b5c313e47c0
buffalojoe <42217874+buffalojoe@users.noreply.github.com>
2025-03-15 14:44:58 -0400

    Introduced game logging functionality

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 # Finds a random word from a dictionary of words
 def findWord(dictionary):
@@ -12,23 +13,42 @@ def findWord(dictionary):
     index = random.randint(0, len(words) - 1)
     while len(words[index]) < 5:
         index = random.randint(0, len(words) - 1)
-    
-    return words[index]
 
+    foundWord = [" "] * len(words[index])
+    
+    return (words[index], foundWord)
+
+# Select a mode that will determine the number of guesses allowed
 def modeSelect(word):
     mode = input("Select a difficulty:\nEasy\nMedium\nHard\n").lower()
 
     if mode == "easy" or mode == "e":
-        guessLimit = len(word) + 15
+        guessLimit = len(word) + 12
+        mode = "easy"
     elif mode == "medium" or mode == "m":
-        guessLimit = len(word) + 8
+        guessLimit = len(word) + 6
+        mode = "medium"
     elif mode == "hard" or mode == "h":
         guessLimit = len(word) + 1
+        mode = "hard"
     else:
         print("Invalid mode selection")
-        return None
+        sys.exit()
     
-    return guessLimit
+    return (guessLimit, mode)
+
+# Log results of a game of hangman
+def logResult(word, mode, guessCount, guessLimit, result):
+    file = open("hangmanResults.txt", "a")
+    resultMap = {"Word" : word, "Mode" : mode, "Guess Count" : guessCount, "Guess Limit" : guessLimit, "Result": result}
+    result = ""
+    for key in resultMap.keys():
+        result += key + " : " + str(resultMap[key]) + ", "
+    
+    result = result[:-2]
+    file.write(result + "\n")
+    file.close()
+    return None
 
 # Play the hangman game
 def guess(word, foundWord, guessCount, guessLimit, guessedLetters):
@@ -37,59 +57,52 @@ def guess(word, foundWord, guessCount, guessLimit, guessedLetters):
 
     if guessCount == guessLimit:
         print(f"All out of guesses. You lose! The word was {word}")
-        return None
+        return ("Lost", guessCount)
     
     if "".join(foundWord) == word:
         print("You got it! You Win!")
-        return None
+        return ("Won", guessCount)
     
     userGuess = input("Guess a letter: ")
 
     if not userGuess.isalpha():
         print("Letters only!")
-        guess(word, foundWord, guessCount, guessLimit, guessedLetters)
-        return None
+        return guess(word, foundWord, guessCount, guessLimit, guessedLetters)
 
     if len(userGuess) == 1:
         if userGuess in guessedLetters:
             print("You already guessed that letter!")
-            guess(word, foundWord, guessCount, guessLimit, guessedLetters)
-            return None
+            return guess(word, foundWord, guessCount, guessLimit, guessedLetters)
         else:
             guessedLetters.append(userGuess)
     
     guessCount += 1
     if userGuess == word:
-        print("You got it!")
-        return None 
+        print("You got it! You Win!")
+        return ("Won", guessCount)
 
     for i in range(len(word)):
         if userGuess == word[i]:
             foundWord[i] = userGuess
 
-    guess(word, foundWord, guessCount, guessLimit, guessedLetters)
+    return guess(word, foundWord, guessCount, guessLimit, guessedLetters)
 
 # Run the program
 def main():
     dictionary = "dictionary.txt"
-    word = findWord(dictionary)
+    word, foundWord = findWord(dictionary)
 
-    guessLimit = modeSelect(word)
-    if guessLimit == None:
-        return None
+    guessLimit, mode = modeSelect(word)
 
     guessCount = 0
     guessedLetters = []
-    foundWord = []
-
-    for i in range(len(word)):
-        foundWord.append(" ")
 
     print("INSTRUCTIONS:")
     print("Guess a letter or the word on each turn")
     print(f"Each guess adds to your guess count. You only get {guessLimit} guesses!")
     print(f"Your word is {len(word)} letters long\n")
 
-    guess(word, foundWord, guessCount, guessLimit, guessedLetters)
+    result, guessCount = guess(word, foundWord, guessCount, guessLimit, guessedLetters)
+    logResult(word, mode, guessCount, guessLimit, result)
 
 main()
